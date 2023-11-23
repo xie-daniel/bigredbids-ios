5b404041b9b0f852ef4ce2debebe899b77324d75
Daniel Xie <dxie18@gmail.com>
2023-11-23 18:29:10 -0500

    Default XCode Template

---
+++ bigredbids/bigredbids/bigredbidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bigredbidsApp.swift
+//  bigredbids
+//
+//  Created by Daniel Xie on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct bigredbidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
